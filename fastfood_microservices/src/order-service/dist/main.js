da2638f55af5bee57284e5271caafd1ae0679559
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2025-09-14 22:41:22 +0700

    fix dockerfile

--- fastfood_microservices/src/order-service/dist/main.js
+++ fastfood_microservices/src/order-service/dist/main.js
@@ -4,7 +4,9 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    await app.listen(3001);
+    const port = process.env.PORT || 3001;
+    await app.listen(port, '0.0.0.0');
+    console.log(`Order Service listening on port ${port}`);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
