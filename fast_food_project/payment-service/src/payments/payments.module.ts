2668b21c3ce65a9f9139bddf1ba788b4a9236927
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2025-09-23 13:57:29 +0700

    fix paymentservice sync message patterns

--- fast_food_project/payment-service/src/payments/payments.module.ts
+++ fast_food_project/payment-service/src/payments/payments.module.ts
@@ -4,18 +4,8 @@ import { PaymentsService } from './payments.service';
 import { PaymentsController } from './payments.controller';
 import { Payment, PaymentSchema } from './schemas/payment.schema';
 
-// kiểm tra biến môi trường trước
-const mongoUri = process.env.MONGO_URI;
-if (!mongoUri) {
-  throw new Error('MONGO_URI is not set');
-}
-
 @Module({
   imports: [
-    // kết nối đến MongoDB
-    MongooseModule.forRoot(mongoUri),
-
-    // đăng ký schema Payment
     MongooseModule.forFeature([{ name: Payment.name, schema: PaymentSchema }]),
   ],
   controllers: [PaymentsController],
