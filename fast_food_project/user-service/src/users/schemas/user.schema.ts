a4f8f8854d1f271ac2770faf8f106ad0aa6a98be
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2025-09-24 23:57:14 +0700

    give name for colection

--- fast_food_project/user-service/src/users/schemas/user.schema.ts
+++ fast_food_project/user-service/src/users/schemas/user.schema.ts
@@ -3,7 +3,7 @@ import { Document } from 'mongoose';
 
 export type UserDocument = User & Document;
 
-@Schema({ timestamps: true })
+@Schema({ timestamps: true, collection: 'Users' })
 export class User {
   @Prop({ required: true })
   name: string;
