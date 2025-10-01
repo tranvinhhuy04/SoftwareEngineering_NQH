276d8eb7695c64e3154c6b3ddf78b185353bd9a6
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2025-10-01 12:52:14 +0700

    add enum for user and delivery

---
+++ BE/user-service/src/users/enum/user-type.enum.ts
@@ -0,0 +1,7 @@
+// src/users/enums/user-type.enum.ts
+export enum UserType {
+  CUSTOMER = 'customer',
+  RESTAURANT_ADMIN = 'restaurant-admin',
+  STAFF = 'staff',
+  DELIVERY_PERSONNEL = 'delivery-personnel',
+}
