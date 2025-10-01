c65a779d47627ec0f3bcf7e0d0fd4bda90cf5802
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2025-10-01 12:51:17 +0700

    add update and crear userDTO

---
+++ BE/user-service/src/users/dto/user/create-user.dto.ts
@@ -0,0 +1,13 @@
+import { UserActive } from "../../enum/user-active.enum";
+import { UserType } from "../../enum/user-type.enum";
+
+export class CreateUserDto {
+    readonly ID: string;
+    readonly name: string;
+    readonly email: string;
+    readonly password: string;
+    readonly phone: string
+    readonly address: string
+    readonly userType: UserType
+    readonly active: UserActive
+}
