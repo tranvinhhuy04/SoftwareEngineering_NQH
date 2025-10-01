869c69b10769432e9572d51191c713ec3432ea40
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2025-10-01 09:49:14 +0700

    fix run in local

--- BE/user-service/src/main.ts
+++ BE/user-service/src/main.ts
@@ -1,17 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Transport } from '@nestjs/microservices';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice(AppModule, {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
     transport: Transport.RMQ,
     options: {
-      urls: ['amqp://guest:guest@rabbitmq:5672'], // chú ý "rabbitmq" phải đúng service name trong docker-compose
+      urls: [process.env.RABBITMQ_URI || 'amqp://localhost:5672'],
       queue: 'users_queue',
       queueOptions: { durable: false },
     },
   });
 
   await app.listen();
+    console.log('User is running....');
+
 }
 bootstrap();
