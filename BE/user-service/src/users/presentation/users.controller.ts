25ea10d8f6d3f671508ee3a3fe8e44df9ecbe8d4
TyTy7023 <147785705+TyTy7023@users.noreply.github.com>
2025-10-06 14:41:04 +0700

    add getall and getbyid data controller

--- BE/user-service/src/users/presentation/users.controller.ts
+++ BE/user-service/src/users/presentation/users.controller.ts
@@ -3,13 +3,34 @@ import { CreateUserUseCase } from "../application/use-cases/createUser.usecase";
 import { MessagePattern, Payload } from "@nestjs/microservices";
 import { CreateUserDto } from "../application/dto/user/create-user.dto";
 import { UserEntity } from "../domain/entities/user.entity";
+import { GetAllUsersUseCase } from "../application/use-cases/getAllUsers.usecase";
+import { GetUserByIdUseCase } from "../application/use-cases/getUserById.usecase";
+import { SearchUserUseCase } from "../application/use-cases/searchUser.usecase";
 
 @Controller()
 export class UserController {
-  constructor(private readonly createUserUseCase: CreateUserUseCase) {}
+  constructor(
+    private readonly createUserUseCase: CreateUserUseCase,
+    private readonly getAllUsersUseCase: GetAllUsersUseCase,
+    private readonly getUserByIdUseCase: GetUserByIdUseCase,
+    private readonly searchUserUseCase: SearchUserUseCase,
+  
+  ) {}
 
   @MessagePattern({ cmd: 'create_user' })
   async createUser(@Payload() dto: CreateUserDto): Promise<UserEntity> {
     return this.createUserUseCase.execute(dto);
   }
+
+  @MessagePattern({ cmd: 'get_all_users' })
+  async getAllUsers(): Promise<UserEntity[]> {
+    return this.getAllUsersUseCase.execute();
+  }
+
+  @MessagePattern({ cmd: 'get_user_by_id' })
+  async getUserById(@Payload() userId: string): Promise<UserEntity> {
+    return this.getUserByIdUseCase.execute(userId);
+  }
+
+  
 }
